dc5be105ce0e651f99f6c455c21d7d54e08cf855
AlexRazor1337 <alexrazor1337@gmail.com>
2021-07-02 21:26:10 +0300

    Add geo API

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const request = require('postman-request');
 require('dotenv').config();
 
-const url = 'http://api.weatherstack.com/current?access_key=' + process.env.api_key + '&query=37.86,-122.22';
-request(url, (error, response, body) => {
-    const data = JSON.parse(body);
-    console.log(data.current);
+const url = 'http://api.weatherstack.com/current?access_key=' + process.env.weather_api_key + '&query=37.86,-122.22&units=m';
+request({"url": url, "json": true}, (error, response, body) => {
+    console.log(body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain!');
+});
+
+let search_term = 'Kiev';
+const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURI(search_term) + '.json?limit=1&access_token=' + process.env.geo_api_key;
+request({"url": url, "json": true}, (error, response, body) => {
+    console.log(body.features[0].place_name, body.features[0].center);
 });
