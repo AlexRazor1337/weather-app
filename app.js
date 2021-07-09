da72f70cc477fd95256003ae772b6230bfbeaa5d
AlexRazor1337 <alexrazor1337@gmail.com>
2021-07-09 19:51:29 +0300

    Command line input for location

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ require('dotenv').config();
 const geocode = require('./utils/geocode');
 const weather = require('./utils/weather');
 
-geocode('Kiev', (error, data) => {
-    if (error) return console.log(error);
+const location = process.argv[2];
+if (!location) return console.log('Specify a location!');
+else {
+    geocode(location, (error, data) => {
+        if (error) return console.log(error);
 
-    weather(data.latitude, data.longitude, (error, weather_data) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(data.name + ': ' + weather_data.current.weather_descriptions + '. It is currently ' + weather_data.current.temperature + ' degrees out. There is a ' + weather_data.current.precip + '% chance of rain!');
-        }
+        weather(data.latitude, data.longitude, (error, weather_data) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(data.name + ': ' + weather_data.current.weather_descriptions + '. It is currently ' + weather_data.current.temperature + ' degrees out. There is a ' + weather_data.current.precip + '% chance of rain!');
+            }
+        });
     });
-});
-
+}
