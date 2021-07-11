d03f677d8e9ebea38bde8980ad7fcf80fd210ff2
AlexRazor1337 <alexrazor1337@gmail.com>
2021-07-11 15:22:27 +0300

    Install express and hbs, setup express server and hbs views

--- src/app.js
+++ src/app.js
@@ -1,25 +1,44 @@
-require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const weather = require('./utils/weather');
 
-const location = process.argv[2];
-if (!location) return console.log('Specify a location!');
-else {
-    geocode(location, (error, {
-        name,
-        latitude,
-        longitude
-    } = {}) => {
-        if (error) return console.log(error);
+const app = express();
+require('dotenv').config();
 
-        weather(latitude, longitude, (error, {
-            current
-        }) => {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(name + ': ' + current.weather_descriptions + '. It is currently ' + current.temperature + ' degrees out. There is a ' + current.precip + '% chance of rain!');
-            }
-        });
-    });
-}
+const public = path.join(__dirname, '..', 'public');
+const views = path.join(__dirname, '..', 'templates/views');
+const partials = path.join(__dirname, '..', 'templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', views);
+hbs.registerPartials(partials);
+app.use(express.static(public))
+
+
+// const location = process.argv[2];
+// if (!location) return console.log('Specify a location!');
+// else {
+//     geocode(location, (error, {
+//         name,
+//         latitude,
+//         longitude
+//     } = {}) => {
+//         if (error) return console.log(error);
+
+//         weather(latitude, longitude, (error, {
+//             current
+//         }) => {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 console.log(name + ': ' + current.weather_descriptions + '. It is currently ' + current.temperature + ' degrees out. There is a ' + current.precip + '% chance of rain!');
+//             }
+//         });
+//     });
+// }
+
+app.listen(process.env.port, () => {
+    console.log('Server is up on port ' + process.env.port + '!');
+});
