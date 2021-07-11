090db19687d40deecb22dda739e53b6aed026128
AlexRazor1337 <alexrazor1337@gmail.com>
2021-07-11 15:27:20 +0300

    add public content, change header, add about view

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+const weather_form = document.querySelector('form');
+const forecast = document.querySelector('#forecast');
+
+weather_form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let search = document.querySelector('#location_input').value;
+    const url = window.location.href.replaceAll('/?', '/') + 'weather?address=' + encodeURIComponent(search);
+
+    forecast.innerHTML = '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>'
+
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+                forecast.innerHTML = data.error;
+            } else {
+                console.log(data);
+                forecast.innerHTML = data.location + ': ' + data.description + '. It is currently ' + data.temperature + ' degrees out. There is a ' + data.precip + '% chance of rain!';
+            }
+        });
+    });
+});
