f48a564dc9dedc6f7822b77125ba8b15a6ed38a5
AlexRazor1337 <alexrazor1337@gmail.com>
2021-07-11 16:05:59 +0300

    Add cache for weather api calls using JS cookie lib

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,27 @@
 const weather_form = document.querySelector('form');
 const forecast = document.querySelector('#forecast');
 
+const callback = (data) => {
+    forecast.innerHTML = data.location + ': ' + data.description + '. It is currently ' + data.temperature + ' degrees out. There is a ' + data.precip + '% chance of rain!';
+}
+
 weather_form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let search = document.querySelector('#location_input').value;
-    const url = window.location.href.replaceAll('/?', '/') + 'weather?address=' + encodeURIComponent(search);
+    const search = document.querySelector('#location_input').value.toLowerCase();
     forecast.innerHTML = '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>'
 
+    const cached_data = Cookies.get(search);
+    if (cached_data) return callback(JSON.parse(cached_data));
+
+    const url = window.location.href.replaceAll('/?', '/') + 'weather?address=' + encodeURIComponent(search);
     fetch(url).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 forecast.innerHTML = data.error;
             } else {
-                forecast.innerHTML = data.location + ': ' + data.description + '. It is currently ' + data.temperature + ' degrees out. There is a ' + data.precip + '% chance of rain!';
+                Cookies.set(search, data, {expires: 1/96, sameSite: 'Lax'}); // 15 minute cache to save some API calls
+                callback(data);
             }
         });
     });
